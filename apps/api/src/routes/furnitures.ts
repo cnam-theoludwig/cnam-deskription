9734a2e7dd350dfe960487c01206247b51642cd5
Théo LUDWIG <contact@theoludwig.fr>
2025-05-12 08:50:38 +0200

    feat: show relations data instead of ids

--- apps/api/src/routes/furnitures.ts
+++ apps/api/src/routes/furnitures.ts
@@ -1,6 +1,7 @@
 import { database } from "@repo/models/database"
 import {
   FurnitureCreateZodObject,
+  FurnitureWithRelations,
   FurnitureZod,
   FurnitureZodObject,
 } from "@repo/models/Furniture"
@@ -10,11 +11,28 @@ import { publicProcedure } from "../oRPC"
 export const furnitures = {
   get: publicProcedure
     .route({ method: "GET", path: "/furnitures", tags: ["Furniture"] })
-    .output(z.array(FurnitureZodObject))
+    .output(z.array(FurnitureWithRelations))
     .handler(async () => {
       const furnitures = await database
         .selectFrom("Furniture")
-        .selectAll()
+        .innerJoin("State", "Furniture.stateId", "State.id")
+        .innerJoin("Type", "Furniture.typeId", "Type.id")
+        .innerJoin("Location", "Furniture.locationId", "Location.id")
+        .innerJoin("Room", "Location.roomId", "Room.id")
+        .innerJoin("Storey", "Room.storeyId", "Storey.id")
+        .innerJoin("Building", "Storey.buildingId", "Building.id")
+        .select([
+          "Furniture.id",
+          "Furniture.name",
+          "Furniture.locationId",
+          "Furniture.stateId",
+          "Furniture.typeId",
+          "State.name as state",
+          "Type.name as type",
+          "Building.name as building",
+          "Storey.name as storey",
+          "Room.name as room",
+        ])
         .execute()
       return furnitures
     }),
@@ -22,12 +40,34 @@ export const furnitures = {
   create: publicProcedure
     .route({ method: "POST", path: "/furnitures", tags: ["Furniture"] })
     .input(FurnitureCreateZodObject)
-    .output(FurnitureZodObject)
+    .output(FurnitureWithRelations)
     .handler(async ({ input }) => {
-      const furniture = await database
+      const { id } = await database
         .insertInto("Furniture")
         .values(input)
-        .returningAll()
+        .returning(["id"])
+        .executeTakeFirstOrThrow()
+      const furniture = await database
+        .selectFrom("Furniture")
+        .innerJoin("State", "Furniture.stateId", "State.id")
+        .innerJoin("Type", "Furniture.typeId", "Type.id")
+        .innerJoin("Location", "Furniture.locationId", "Location.id")
+        .innerJoin("Room", "Location.roomId", "Room.id")
+        .innerJoin("Storey", "Room.storeyId", "Storey.id")
+        .innerJoin("Building", "Storey.buildingId", "Building.id")
+        .select([
+          "Furniture.id",
+          "Furniture.name",
+          "Furniture.locationId",
+          "Furniture.stateId",
+          "Furniture.typeId",
+          "State.name as state",
+          "Type.name as type",
+          "Building.name as building",
+          "Storey.name as storey",
+          "Room.name as room",
+        ])
+        .where("Furniture.id", "=", id)
         .executeTakeFirstOrThrow()
       return furniture
     }),
