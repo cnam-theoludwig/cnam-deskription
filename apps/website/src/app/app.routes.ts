2bf53550f58d824355178fd486eb04d4e81974d2
Mathys PIERREZ <mathyspierrez2@gmail.com>
2025-06-07 10:24:51 +0200

    feat(ui): improve form layout, reset handling and improve UI in general (#7)

--- apps/website/src/app/app.routes.ts
+++ apps/website/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import type { Routes } from "@angular/router"
-import { HomePageComponent } from "../pages/home-page/home-page.component"
+//import { HomePageComponent } from "../pages/home-page/home-page.component"
 import { FurnituresPageComponent } from "../pages/furnitures-page/furnitures-page.component"
 
 export const routes: Routes = [
+  // {
+  //   path: "",
+  //   component: HomePageComponent,
+  // },
   {
     path: "",
-    component: HomePageComponent,
-  },
-  {
-    path: "furnitures",
     component: FurnituresPageComponent,
   },
   {
