4c3522dfe4c6da7a4e7e93997e4aa2a7ecef543d
Danaé A.M. <dev.danae@gmail.com>
2025-09-26 16:22:10 +0200

    feat(ui): Install PrimeNG

--- apps/website/src/app/app.config.ts
+++ apps/website/src/app/app.config.ts
@@ -5,10 +5,22 @@ import { provideRouter } from "@angular/router"
 import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http"
 import { routes } from "./app.routes"
 
+import { providePrimeNG } from "primeng/config"
+import Aura from "@primeuix/themes/aura"
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: ".my-app-dark",
+        },
+      },
+    }),
   ],
 }
