5bbd85c1c8e694b235a8925c7b053d73e75c239b
BRENNER Quentin <brenner.quentin@gmail.com>
2025-12-19 14:08:14 +0100

    fix: database migrate for storey plan

--- packages/models/src/database/migrations/1766148279789_storey_add_floor_plan.ts
+++ packages/models/src/database/migrations/1766148279789_storey_add_floor_plan.ts
@@ -1,12 +1,12 @@
 import type { Kysely } from "kysely"
 
-export async function up(db: Kysely<any>): Promise<void> {
+export const up = async (db: Kysely<any>): Promise<void> => {
   await db.schema
     .alterTable("Storey")
     .addColumn("floorPlanImage", "text")
     .execute()
 }
 
-export async function down(db: Kysely<any>): Promise<void> {
+export const down = async (db: Kysely<any>): Promise<void> => {
   await db.schema.alterTable("Storey").dropColumn("floorPlanImage").execute()
 }
